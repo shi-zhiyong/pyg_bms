00a8f1497214cc4112ec398d9e92c9f26be7f8d2
szy <503305354@qq.com>
2019-02-13 17:26:14 +0800

    ??

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,26 @@
 <template>
-    <div>login----------</div>
+  <el-form :label-position="labelPosition" label-width="80px" :model="formdata">
+    <el-form-item label="用户名">
+      <el-input v-model="formdata.name"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="formdata.password"></el-input>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
   export default {
-    name: 'login'
+    name: 'login',
+    data(){
+      return {
+        labelPosition:"top",
+        formLabelAlign:{
+          name:"",
+          password:""
+        }
+      }
+    }
   }
 </script>
 
