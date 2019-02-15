c1fc872d7d363ef3800846ccd699224d14517be4
szy <503305354@qq.com>
2019-02-15 08:12:15 +0800

    2019-1-15项目准备

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ import router from './router'
 
 //引入axios
 import axios from 'axios'
-axios.defaults.baseURL = "http://localhost:8888/api/private/v1/"
-Vue.prototype.$http = axios
 
 //引入公共样式
 import '@/assets/css/base.css'
+axios.defaults.baseURL = "http://localhost:8888/api/private/v1/"
+Vue.prototype.$http = axios
 
 // ElementUI其实Vue插件,和VueRouter用法一样  Vue.use(名)
 // Vue插件的使用
